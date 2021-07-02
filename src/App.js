c706bb9a8d0ddb7a2f7c97f0378309d19029ef56
hdinjos <hdinjos@gmail.com>
2021-07-03 05:23:29 +0700

    make function counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+const s = {
+  wrap: {
+    display:"flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  containerAct: {
+    display:"flex",
+    fontSize: "30px"
+  },
+  action: {
+    width:"50px",
+    height: "50px",
+    backgroundColor: "#eee",
+    textAlign: "center",
+    lineHeight: "50px",
+    cursor:"pointer",
+    border:"1px solid black"
+  },
+  number: {
+    width:"80px",
+    height: "50px",
+    lineHeight:"50px",
+    textAlign: "center",
+    borderBottom:"1px solid black",
+    borderTop:"1px solid black"
+  },
+
+}
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const onClickPlus = () => {
+    if(value < 10 ){
+      setValue(value + 1);
+      setMessage("")
+    }else {
+      setMessage("Max!");
+    }
+  };
+
+  const onClickMinus = () => {
+    if(value > 0) {
+      setValue(value -1);
+      setMessage("")
+    }else {
+      setMessage("Min!");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={s.wrap}>
+     <h3>Counter Jos</h3>
+     <div style={s.containerAct}>
+       <div onClick={()=> onClickMinus()} style={s.action}>-</div>
+       <div style={s.number}>{value}</div>
+       <div onClick={()=> onClickPlus()} style={s.action}>+</div>
+     </div>
+     <div><h4>{message}</h4></div>
     </div>
   );
 }
