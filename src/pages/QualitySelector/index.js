6dd607a8e0500c79d91a64c1847e94596c589b92
hdinjos <hdinjos@gmail.com>
2021-07-04 23:20:16 +0700

    change ui

--- src/pages/QualitySelector/index.js
+++ src/pages/QualitySelector/index.js
@@ -1,17 +1,29 @@
-import useQualitySelector from './useQualitySelector';
-import s from './style';
+import useQualitySelector from "./useQualitySelector";
+import s from "./style";
 
 function QualitySelector() {
-  const {onClickPlus, onClickMinus, value, message} = useQualitySelector();
+  const { onClickPlus, onClickMinus, value, message } = useQualitySelector();
   return (
     <div style={s.wrap}>
-     <h3>Counter Jos</h3>
-     <div style={s.containerAct}>
-       <div onClick={()=> onClickMinus()} style={s.action}>-</div>
-       <div style={s.number}>{value}</div>
-       <div onClick={()=> onClickPlus()} style={s.action}>+</div>
-     </div>
-     <div><h4>{message}</h4></div>
+      <h3>Counter Jos</h3>
+      <div style={s.containerAct}>
+        <div
+          onClick={() => onClickMinus()}
+          style={value === 0 ? s.noAllowed : s.action}
+        >
+          -
+        </div>
+        <div style={s.number}>{value}</div>
+        <div
+          onClick={() => onClickPlus()}
+          style={value === 10 ? s.noAllowed : s.action}
+        >
+          +
+        </div>
+      </div>
+      <div>
+        <h4>{message}</h4>
+      </div>
     </div>
   );
 }
--- src/pages/QualitySelector/style.js
+++ src/pages/QualitySelector/style.js
@@ -1,31 +1,41 @@
 const s = {
   wrap: {
-    display:"flex",
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   containerAct: {
-    display:"flex",
-    fontSize: "30px"
+    display: "flex",
+    fontSize: "30px",
   },
   action: {
-    width:"50px",
+    width: "50px",
+    height: "50px",
+    backgroundColor: "rgb(211, 211, 211)",
+    textAlign: "center",
+    lineHeight: "50px",
+    cursor: "pointer",
+    border: "1px solid black",
+  },
+  number: {
+    width: "80px",
+    height: "50px",
+    lineHeight: "50px",
+    textAlign: "center",
+    borderBottom: "1px solid black",
+    borderTop: "1px solid black",
+  },
+  noAllowed: {
+    color: "rgb(113, 113, 113)",
+    width: "50px",
     height: "50px",
     backgroundColor: "#eee",
     textAlign: "center",
     lineHeight: "50px",
-    cursor:"pointer",
-    border:"1px solid black"
+    border: "1px solid black",
+    cursor: "not-allowed",
   },
-  number: {
-    width:"80px",
-    height: "50px",
-    lineHeight:"50px",
-    textAlign: "center",
-    borderBottom:"1px solid black",
-    borderTop:"1px solid black"
-  },
-}
+};
 
 export default s;
